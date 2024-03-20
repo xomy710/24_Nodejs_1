610f6afd0144b6089691b2f876f05b360a52c78f
xomy <xomy710@gmail.com>
2024-03-20 19:00:52 +0900

    test2

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
 })
 
-app.get('/', (요청, 응답) => {
-  응답.send('hihi')
+app.get('/', (req, res) => {
+  res.send('hihi')
 }) 
