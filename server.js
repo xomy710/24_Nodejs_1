19069b1ba475177b433851682b412a3e2d6b0a05
xomy <xomy710@gmail.com>
2024-03-22 21:40:52 +0900

    test2

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/jjeong'))
+
+
 app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
 })
 
 app.get('/', (req, res) => {
-  res.send('hihi')
+  res.sendfile(__dirname + '/index.html')
+}) 
+
+app.get('/about', (req, res) => {
+  res.sendfile(__dirname + '/about.html')
+}) 
+
+app.get('/news', (req, res) => {
+  res.send('today news is ...')
+}) 
+
+app.get('/shop', (req, res) => {
+  res.send('this is shopping page ...')
 }) 
